5038bb3cff4bb436530b1d240f09e1f47adfcecd
Alessandro Ercolani <a.ercolani@alumni.unitn.it>
2017-03-10 14:12:41 +0100

    Exception fixed

--- old_hdfs_master_crawler.py
+++ old_hdfs_master_crawler.py
@@ -7,6 +7,7 @@ import pprint
 import redis
 import os
 from hdfs import InsecureClient
+import hdfs
 #from confluent_kafka import Producer
 #from hdfs import TokenClient
 #from hdfs import InsecureClient
@@ -15,7 +16,7 @@ from hdfs import InsecureClient
 #client = InsecureClient('http://node-1.testing:50070/', 'root', root='/user/root/open_data')
 #request = urllib2.Request("http://93.63.32.36/api/3/action/group_list")
 #URL_DATI_TRENTINO = "http://dati.trentino.it"
-URL_DATI_GOV = "http://93.63.32.36"
+URL_DATI_GOV = "http://www.datigov.it"
 #URL_DATI_GOV = "http://192.168.22.11/"
 
 class HeadRequest(urllib2.Request):
@@ -29,9 +30,15 @@ class MasterCrawler:
         self.r = redis.StrictRedis(host=redis_ip, port=redis_port, db=0)
         self.client =  InsecureClient('http://cdh1:50070/', 'admin', root='/user/admin/open_data')
 
-    def formatUrl(self, url):
+    def formatUrl(url):
         urlSplit = url.rsplit('/', 1)
-        urlEnd = urllib.quote(urlSplit[1])
+        lastPart = urlSplit[1]
+        urlEnd = ""
+        if "?" in lastPart:
+            splitted = lastPart.split("?")
+            urlEnd = splitted[0] + "?" + urllib.quote(splitted[1])
+        else:
+            urlEnd = urllib.quote(lastPart)
         urlStart = urlSplit[0]
         finalUrl = urlStart + "/" + urlEnd
         return finalUrl
@@ -42,8 +49,8 @@ class MasterCrawler:
             with self.client.write('old_dati_gov/dati_gov.json', encoding='utf-8') as writer:
                 writer.write('')
         request = urllib2.Request(URL_DATI_GOV + "/api/3/action/package_list")
-        base64string = base64.b64encode('%s:%s' % ("newdatigovit", "qaz-plm-17"))
-        request.add_header("Authorization", "Basic %s" % base64string)
+        #base64string = base64.b64encode('%s:%s' % ("newdatigovit", "qaz-plm-17"))
+        #request.add_header("Authorization", "Basic %s" % base64string)
         response = urllib2.urlopen(request)
         assert response.code == 200
         response_dict = json.loads(response.read())
@@ -61,14 +68,20 @@ class MasterCrawler:
             dataset_id = red.lpop("old_dataset_id")
             encRes = urllib.urlencode({"id" : unicode(dataset_id).encode('utf-8')})
             request_info = urllib2.Request(URL_DATI_GOV + "/api/3/action/package_show?" + encRes)
-            base64string = base64.b64encode('%s:%s' % ("newdatigovit", "qaz-plm-17"))
-            request_info.add_header("Authorization", "Basic %s" % base64string)
+            #base64string = base64.b64encode('%s:%s' % ("newdatigovit", "qaz-plm-17"))
+            #request_info.add_header("Authorization", "Basic %s" % base64string)
+            info = None
             try:
                 response_info = urllib2.urlopen(request_info)
                 info_dataset = json.loads(response_info.read())
                 results = info_dataset['result']
                 info = results
+            except Exception, e:
+                print str(e)
+                print "o qui e perche"
+                red.lpush("old_dataset_error", dataset_id)
                 #print json.dumps(info)
+            if info:
                 if 'resources' in info:
                     #print info
                     info["m_status_resources"] = "ok"
@@ -80,9 +93,12 @@ class MasterCrawler:
                         rFormat = resource['format']
                         rName = resource['name']
                         rId = resource['id']
+                        print rUrl
                         finalUrl = self.formatUrl(rUrl)
                         rInfo = urllib2.Request(finalUrl)
+                        rInfo = urllib2.Request(rUrl)
                         rInfo.get_method = lambda : 'HEAD'
+                        #rInfo.add_header("Authorization", "Basic %s" % base64string)
                         try:
                             rReq = urllib2.urlopen(rInfo)
                             if rReq.code == 200:
@@ -90,21 +106,24 @@ class MasterCrawler:
                             else:
                                 resource["m_status"] = "ko"
                         except Exception, e:
+                            print info
+                            print "sara questo????"
                             resource["m_status"] = "ko"
+                            print "sara qui ..."
                             print str(e)
                 else:
-                    print info
-                    info["m_status_resources"] = "ko"
-                    print "NO RESOURCES"
-                with self.client.write('old_dati_gov/dati_gov.json', encoding='utf-8', append=True) as writer:
+                        info["m_status_resources"] = "ko"
+                        print "NO RESOURCES"
+                try:
+                    with self.client.write('old_dati_gov/dati_gov.json', encoding='utf-8', append=True) as writer:
                         writer.write(json.dumps(info) + '\n')
-            except Exception, e:
-                print str(e)
-                red.lpush("old_dataset_error", dataset_id)
+                except hdfs.util.HdfsError, ex:
+                    print str(ex)
+
 
 
 #URL_DATI_TRENTINO = "http://dati.trentino.it"
-URL_DATI_GOV = "http://93.63.32.36"
+URL_DATI_GOV = "http://www.dati.gov.it"
 #URL_NEW_DATI_GOV = "http://192.168.22.11/"
 REDIS_IP = "192.168.22.12"
 REDIS_PORT = 6379
--- old_hdfs_worker_crawler.py
+++ old_hdfs_worker_crawler.py
@@ -6,6 +6,7 @@ import base64
 import pprint
 import redis
 import os
+import hdfs
 from hdfs import InsecureClient
 #from confluent_kafka import Producer
 #from hdfs import TokenClient
@@ -15,8 +16,8 @@ from hdfs import InsecureClient
 #client = InsecureClient('http://node-1.testing:50070/', 'root', root='/user/root/open_data')
 #request = urllib2.Request("http://93.63.32.36/api/3/action/group_list")
 #URL_DATI_TRENTINO = "http://dati.trentino.it"
-#URL_DATI_GOV = "http://93.63.32.36"
-URL_DATI_GOV = "http://192.168.22.11/"
+URL_DATI_GOV = "http://www.dati.gov.it"
+#URL_DATI_GOV = "http://192.168.22.11/"
 
 class HeadRequest(urllib2.Request):
      def get_method(self):
@@ -30,9 +31,15 @@ class WorkerCrawler:
         self.r = redis.StrictRedis(host=redis_ip, port=redis_port, db=0)
         self.client =  InsecureClient('http://cdh1:50070/', 'admin', root='/user/admin/open_data')
 
-    def formatUrl(self, url):
+    def formatUrl(url):
         urlSplit = url.rsplit('/', 1)
-        urlEnd = urllib.quote(urlSplit[1])
+        lastPart = urlSplit[1]
+        urlEnd = ""
+        if "?" in lastPart:
+            splitted = lastPart.split("?")
+            urlEnd = splitted[0] + "?" + urllib.quote(splitted[1])
+        else:
+            urlEnd = urllib.quote(lastPart)
         urlStart = urlSplit[0]
         finalUrl = urlStart + "/" + urlEnd
         return finalUrl
@@ -43,14 +50,21 @@ class WorkerCrawler:
             dataset_id = red.lpop("old_dataset_id")
             encRes = urllib.urlencode({"id" : unicode(dataset_id).encode('utf-8')})
             request_info = urllib2.Request(URL_DATI_GOV + "/api/3/action/package_show?" + encRes)
-            base64string = base64.b64encode('%s:%s' % ("newdatigovit", "qaz-plm-17"))
-            request_info.add_header("Authorization", "Basic %s" % base64string)
+            #base64string = base64.b64encode('%s:%s' % ("newdatigovit", "qaz-plm-17"))
+            #request_info.add_header("Authorization", "Basic %s" % base64string)
+            info = None
             try:
                 response_info = urllib2.urlopen(request_info)
                 info_dataset = json.loads(response_info.read())
                 results = info_dataset['result']
                 info = results
+            except Exception, e:
+                print str(e)
+                print "o qui e perche"
+                print URL_DATI_GOV + "/api/3/action/package_show?" + encRes
+                red.lpush("old_dataset_error", dataset_id)
                 #print json.dumps(info)
+            if info:
                 if 'resources' in info:
                     #print info
                     info["m_status_resources"] = "ok"
@@ -62,9 +76,12 @@ class WorkerCrawler:
                         rFormat = resource['format']
                         rName = resource['name']
                         rId = resource['id']
+                        print rUrl
                         finalUrl = self.formatUrl(rUrl)
                         rInfo = urllib2.Request(finalUrl)
+                        rInfo = urllib2.Request(rUrl)
                         rInfo.get_method = lambda : 'HEAD'
+                        #rInfo.add_header("Authorization", "Basic %s" % base64string)
                         try:
                             rReq = urllib2.urlopen(rInfo)
                             if rReq.code == 200:
@@ -72,18 +89,19 @@ class WorkerCrawler:
                             else:
                                 resource["m_status"] = "ko"
                         except Exception, e:
+                            print info
+                            print "sara questo????"
                             resource["m_status"] = "ko"
+                            print "sara qui ..."
                             print str(e)
                 else:
-                    print info
-                    info["m_status_resources"] = "ko"
-                    print "NO RESOURCES"
-                with self.client.write('old_dati_gov/dati_gov.json', encoding='utf-8', append=True) as writer:
+                        info["m_status_resources"] = "ko"
+                        print "NO RESOURCES"
+                try:
+                    with self.client.write('old_dati_gov/dati_gov.json', encoding='utf-8', append=True) as writer:
                         writer.write(json.dumps(info) + '\n')
-            except Exception, e:
-                print str(e)
-                red.lpush("old_dataset_error", dataset_id)
-
+                except hdfs.util.HdfsError, ex:
+                    print str(ex)
 
 worker = WorkerCrawler("192.168.22.12", 6379)
 worker.consumeData()
